e3833d110a7536882f70e094e93216ee78625420
Walden Li <admin@waldenli.com>
2021-09-22 12:21:13 -0400

    Change redirect

--- static/js/onboard.js
+++ static/js/onboard.js
@@ -1,21 +1,28 @@
 window.onload = () => {
-    $('#button').mousedown(function (event) {
+    $('#check-btn').mousedown(function (event) {
         switch (event.which) {
             case 2:
-                if (urlParams.length > 0) {
-                    location.href = urlParams.concat('&click=success');
-                } else {
-                    location.href = '&click=success';
-                }
+                // if (urlParams.length > 0) {
+                //     location.href = urlParams.concat('&click=success');
+                // } else {
+                //     location.href = '?click=success';
+                // }
+                location.href = 'launch.php?'.concat(urlParams);
                 break;
             default:
                 alert('Please click the button with your scroll wheel!');
         }
     });
+
+    // $('#consent-btn').click(function () {
+    //     location.href = expLoc;
+    // });
+
     const urlParams = window.location.search;
     const params = new URLSearchParams(urlParams);
+    // const expLoc = params.get('exploc');
     if(params.get('click')==='success') {
-        $('#button').remove();
+        $('#check-btn').remove();
         $('#button-col').append("<p>Thank you!</p>");
         setTimeout( ()=> {
             location.href = 'consent.html'.concat(urlParams)
